afbe85fb66a8f02dbaacc0ca39870f5261dc8a85
Joseph Emerson <11266326+ariesshrimp@users.noreply.github.com>
2023-09-28 21:59:24 +0000

    Switch to mdx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { Header } from "./components";
+import { Header } from "../components";
 import { Gabriela } from "next/font/google";
+import Content from "./content.mdx";
 
 const gabriela = Gabriela({ weight: "400", subsets: ["latin"] });
 
@@ -8,40 +9,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center p-24">
       <Header />
       <div className="prose dark:prose-invert">
-        <p>
-          <span
-            className={`${gabriela.className} align-text-top text-8xl pr-2 font-black float-left`}
-          >
-            H
-          </span>
-          {`i, thanks for being here! It means that you care about what Portland is like, and that you want it to be a healthy, happy place for everyone
-that lives here. Me too! In the coming weeks and months, I'll have a lot more to say about why I'm running, how I hope to earn your trust, and what my vision for that happy, healthy Portland is. For now, I hope you're safe and well wherever you are. I hope you'll join me in imagining the most amazing future possible for Portland.
-`}
-        </p>
-        <p>{`For now I hope you'll check out some other people I support: `}</p>
-        <ul>
-          <li>
-            <a href="https://www.portlandforward.org/" className="underline">
-              The team at Portland Forward
-            </a>
-            {`, who care about making Portland the best possible place for
-            everyone.`}
-          </li>
-          <li>
-            <a href="https://www.starvoting.org/" className="underline">
-              The team at STAR Voting
-            </a>
-            {` who want to ensure that democracy works for everyone and can't be
-            gamed or abused.`}
-          </li>
-          <li>
-            <a href="https://superthank.org/" className="underline">
-              The people at SuperThank
-            </a>
-            {`, who want doing good in our communities to be easier and more
-            rewarding.`}
-          </li>
-        </ul>
+        <Content />
       </div>
     </main>
   );
