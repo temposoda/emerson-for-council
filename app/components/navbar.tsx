adef6233a627c31de99e4726b2ec8e15ae028670
Joseph Emerson <11266326+ariesshrimp@users.noreply.github.com>
2023-10-04 23:33:10 +0000

    Update navbar.tsx and content.mdx

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -12,9 +12,9 @@ export const Navbar = () => (
               class="inline-block w-6 h-6 stroke-current"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLineCap="round"
+                strokeLineJoin="round"
+                strokeWidth="2"
                 d="M4 6h16M4 12h16M4 18h16"
               ></path>
             </svg>
