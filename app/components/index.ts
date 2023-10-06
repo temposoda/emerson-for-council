719c154e3f25dd5666999fcb4d8dcfee88719851
Joseph Emerson <11266326+ariesshrimp@users.noreply.github.com>
2023-10-06 16:39:59 +0000

    delete extraneous form component

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,5 +1,4 @@
 export { Footer } from "./footer";
 export { DonateButton } from "./donate-button";
 export { VolunteerButton } from "./volunteer-button";
-export { VolunteerForm } from "./volunteer-form";
 export { Navbar } from "./navbar";
