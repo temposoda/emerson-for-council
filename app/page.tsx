12d790076aec68b9851aa7d5da6002b13e918092
Joseph Emerson <11266326+ariesshrimp@users.noreply.github.com>
2023-10-22 21:33:58 +0000

    improve heading padding

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
         <div className="hero-content text-center">
           <div className="max-w-md flex flex-col gap-10">
             <div className="flex flex-col justify-start items-center ">
-              <h1 className="border-solid border-t-4 border-b-4 border-current text-accent mb-2 w-full text-7xl md:text-8xl lg:text-9xl uppercase font-bold flex flex-col justify-stretch items-center">
+              <h1 className="border-solid border-t-4 border-b-4 pt-2 pb-4 border-current text-accent mb-2 w-full text-7xl md:text-8xl lg:text-9xl uppercase font-bold flex flex-col justify-stretch items-center">
                 <span className="text-white">Joseph</span>
                 <span className="text-white tracking-tighter text-center">
                   Emerson
