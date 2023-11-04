f1ee0fc6ac1773b941dcd41d80de9bb9c4d46c36
Joseph Emerson <11266326+ariesshrimp@users.noreply.github.com>
2023-11-04 04:35:52 +0000

    Update page.tsx and content.mdx

--- app/donate/page.tsx
+++ app/donate/page.tsx
@@ -1,5 +1,6 @@
 import { permanentRedirect } from "next/navigation";
 
+// https://dpo.org/oregon-political-tax-credit/
 export default async function DonateRedirect() {
   permanentRedirect("https://secure.actblue.com/donate/emersonforcouncil");
 }
