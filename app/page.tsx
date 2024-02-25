52ba1db973c5bedfb531327bad0ebd5606000dd4
Joseph Emerson <github.00uqy@simplelogin.com>
2024-02-25 21:19:44 +0000

    parting words

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,20 @@ export default async function Home() {
           backgroundImage: `url(${AlbertaPark.src})`,
         }}
       >
-        <div className="hero-content text-center text-white">
-          <div className="max-w-lg">
-            <div className="mt-64 flex flex-row gap-2 justify-center">
-              <VolunteerButton />
-            </div>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-left text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Thank You!</h1>
+            <p className="mb-5">
+              I ended my campaign for Portland City Council at the end of
+              January, 2024 to focus on preparing to welcome new twins to my
+              family. I will continue to host this website describing my vision
+              and platform for anyone to borrow. Portland can play an important
+              role in shaping the realm of civic possibility around the world.
+              It can be a safe, healthy, happy place for children and families
+              of all kinds. If you're reading this, I hope you'll give your best
+              effort to realize that possibility, for all of us.
+            </p>
           </div>
         </div>
       </div>
