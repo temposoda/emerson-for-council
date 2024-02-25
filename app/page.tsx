dc0165f831b55af82aced156289ce90cbbfb7ea8
Joseph Emerson <github.00uqy@simplelogin.com>
2024-02-25 21:25:08 +0000

    fix apostrophes

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,14 @@ export default async function Home() {
           <div className="max-w-md">
             <h1 className="mb-5 text-5xl font-bold">Thank You!</h1>
             <p className="mb-5">
-              I ended my campaign for Portland City Council at the end of
+              {`I ended my campaign for Portland City Council at the end of
               January, 2024 to focus on preparing to welcome new twins to my
               family. I will continue to host this website describing my vision
               and platform for anyone to borrow. Portland can play an important
               role in shaping the realm of civic possibility around the world.
               It can be a safe, healthy, happy place for children and families
               of all kinds. If you're reading this, I hope you'll give your best
-              effort to realize that possibility, for all of us.
+              effort to realize that possibility, for all of us.`}
             </p>
           </div>
         </div>
